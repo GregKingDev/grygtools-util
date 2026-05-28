c29dc1ad8277f2cdf7ada0a4278850488a0e1193
GregKingDev <Greg.King.dev@gmail.com>
2026-05-28 13:28:06 -0300

    adding conditional serialized fields and version bump

---
+++ ConditionalSerializedField.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace GrygToolsUtils
+{
+	/// <summary>
+	/// Hides a SerializeField in the editor based the name of another field in the class and comparison value
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class ConditionalSerializedField : PropertyAttribute
+	{
+		public readonly string FieldName;
+		public readonly object Value;
+		public readonly bool Invert;
+
+		/// <summary>
+		/// Hides a SerializeField in the editor based the name of another field in the class and comparison value
+		/// </summary>
+		/// <param name="name">Name of the other field used to determine visibility</param>
+		/// <param name="v">Value the other param is checked against</param>
+		/// <param name="invert">If invert is true field will be hidden when values match</param>
+		public ConditionalSerializedField(string name, object v, bool invert = false)
+		{
+			FieldName = name;
+			Value = v;
+			Invert = invert;
+		}
+	}
+}
---
+++ Editor/ConditionalSerializedFieldEditor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace GrygToolsUtils
+{
+	/// <summary>
+	/// Conditionally shows this serialized field in editor. First field is the name of the property to check second is the value.
+	/// Currently works with bool, string, int, enum, float
+	/// </summary>
+	[CustomPropertyDrawer(typeof(ConditionalSerializedField))]
+	public class ConditionalSerializedFieldEditor : PropertyDrawer
+	{
+		private bool m_PassesCheck = true;
+		private bool m_Inverted = false;
+
+		private bool ShouldShow => (m_Inverted && !m_PassesCheck) || (!m_Inverted && m_PassesCheck); 
+		
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			ConditionalSerializedField localAttribute = (ConditionalSerializedField)this.attribute;
+			string name = localAttribute.FieldName;
+			m_Inverted = localAttribute.Invert;
+
+			SerializedProperty field = property.serializedObject.FindProperty(name);
+
+			switch (field.propertyType)
+			{
+				case SerializedPropertyType.Boolean:
+				{
+					m_PassesCheck = field.boolValue.CompareTo(localAttribute.Value) == 0;
+					break;
+				}
+
+				case SerializedPropertyType.String:
+				{
+					m_PassesCheck = field.stringValue.CompareTo(localAttribute.Value) == 0;
+					break;
+				}
+
+				case SerializedPropertyType.Enum:
+				{
+					m_PassesCheck = field.intValue == (int)localAttribute.Value;
+					break;
+				}
+
+				case SerializedPropertyType.Integer:
+				{
+					m_PassesCheck = field.intValue.CompareTo(localAttribute.Value) == 0;
+					break;
+				}
+
+				case SerializedPropertyType.Float:
+				{
+					m_PassesCheck = field.floatValue.CompareTo(localAttribute.Value) == 0;
+					break;
+				}
+
+				case SerializedPropertyType.ObjectReference:
+				{
+					m_PassesCheck = (field.objectReferenceValue != null).CompareTo(localAttribute.Value) == 0;
+					break;
+				}
+
+				default:
+				{
+					Debug.LogError(
+						$"Unrecognized type of {field.propertyType}. Only int, float, bool, enum and string are supported by ConditionalSerializedField");
+					m_PassesCheck = false;
+					break;
+				}
+			}
+
+			if (ShouldShow)
+			{
+				EditorGUI.PropertyField(position, property, label, true);
+			}
+		}
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			if (ShouldShow)
+			{
+				return EditorGUI.GetPropertyHeight(property);
+			}
+			else
+			{
+				return -EditorGUIUtility.standardVerticalSpacing;
+			}
+		}
+	}
+}
