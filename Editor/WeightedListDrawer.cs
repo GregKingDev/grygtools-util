eb4037af3bdfe90e815387e5600a600d2f4c22f0
GregKingDev <Greg.King.dev@gmail.com>
2026-08-24 13:10:34 -0300

    Fixing line height issue

--- Editor/WeightedListDrawer.cs
+++ Editor/WeightedListDrawer.cs
@@ -10,18 +10,23 @@ namespace GrygToolsUtils
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
 			var entriesProp = property.FindPropertyRelative("m_Entries");
-			return EditorGUI.GetPropertyHeight(entriesProp, true);
+			return EditorGUI.GetPropertyHeight(entriesProp, true) + EditorGUIUtility.singleLineHeight * 2;
 		}
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
 			var totalWeightProp = property.FindPropertyRelative("m_TotalWeight");
+    
+			position.height = EditorGUIUtility.singleLineHeight;  // constrain height
+    
 			EditorGUI.BeginDisabledGroup(true);
 			EditorGUI.PropertyField(position, totalWeightProp);
 			EditorGUI.EndDisabledGroup();
 			position.y += EditorGUI.GetPropertyHeight(totalWeightProp) + EditorGUIUtility.standardVerticalSpacing;
+    
 			var entriesProp = property.FindPropertyRelative("m_Entries");
-			
+			position.height = EditorGUI.GetPropertyHeight(entriesProp, true);  // restore height for entries
+
 			var genericArgs = fieldInfo.FieldType.GetGenericArguments();
 			var typeName = genericArgs.Length > 0 ? genericArgs[0].Name : "Entry";
 			var entriesLabel = new GUIContent($"{typeName} Entries");
