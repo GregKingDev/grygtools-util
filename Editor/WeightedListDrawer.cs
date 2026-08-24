95ef14d039367307825b3e6f09416c08a1431e1b
GregKingDev <Greg.King.dev@gmail.com>
2026-08-24 12:12:13 -0300

    Adding weighted list drawer to having inspector draw all elements of the list instead of boxing them into a scroll area

---
+++ Editor/WeightedListDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+namespace GrygToolsUtils
+{
+	[CustomPropertyDrawer(typeof(WeightedList<>))]
+	public class WeightedListDrawer : PropertyDrawer
+	{
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			var entriesProp = property.FindPropertyRelative("m_Entries");
+			return EditorGUI.GetPropertyHeight(entriesProp, true);
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var totalWeightProp = property.FindPropertyRelative("m_TotalWeight");
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUI.PropertyField(position, totalWeightProp);
+			EditorGUI.EndDisabledGroup();
+			position.y += EditorGUI.GetPropertyHeight(totalWeightProp) + EditorGUIUtility.standardVerticalSpacing;
+			var entriesProp = property.FindPropertyRelative("m_Entries");
+			
+			var genericArgs = fieldInfo.FieldType.GetGenericArguments();
+			var typeName = genericArgs.Length > 0 ? genericArgs[0].Name : "Entry";
+			var entriesLabel = new GUIContent($"{typeName} Entries");
+			EditorGUI.PropertyField(position, entriesProp, entriesLabel, true);
+		}
+	}
+}
